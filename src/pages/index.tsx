ec6c3e46949ce5bb77cb1474aa85c00b6d699750
Oliver Timothy <oliverwakogi@live.com>
2022-08-30 06:38:29 +0300

    :sparkles:: snapping to edges with gesture callbacks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useGesture } from "@use-gesture/react";
+import { Handler, useGesture } from "@use-gesture/react";
 import type { NextPage } from "next";
 import { FC, useRef, useState } from "react";
 
@@ -18,6 +18,34 @@ export default Home;
 const ImageCropper: FC<{ src?: string }> = ({ src }) => {
   const [{ x, y, scale }, setCrop] = useState({ x: 0, y: 0, scale: 1 });
   const imageRef = useRef<HTMLImageElement | null>(null);
+  const imageContainerRef = useRef<HTMLImageElement | null>(null);
+
+  const adjustImage: Handler<
+    "drag" | "pinch",
+    PointerEvent | TouchEvent | MouseEvent | KeyboardEvent
+  > = () => {
+    const _imageRef = imageRef.current as HTMLImageElement;
+    const _imageContainerRef = imageContainerRef.current as HTMLImageElement;
+
+    let newCrop = { x, y, scale };
+    let imageBounds = _imageRef.getBoundingClientRect();
+    let containerBounds = _imageContainerRef.getBoundingClientRect();
+    let originalWidth = _imageRef.clientWidth;
+    let widthOverhang = (imageBounds.width - originalWidth) / 2;
+    let originalHeight = _imageRef.clientHeight;
+    let heightOverhang = (imageBounds.height - originalHeight) / 2;
+
+    if (imageBounds?.left > containerBounds?.left) newCrop.x = widthOverhang;
+    else if (imageBounds.right < containerBounds.right)
+      newCrop.x = -(imageBounds.width - containerBounds.width) + widthOverhang;
+
+    if (imageBounds.top > containerBounds.top) newCrop.y = heightOverhang;
+    else if (imageBounds.bottom < containerBounds.bottom)
+      newCrop.y =
+        -(imageBounds.height - containerBounds.height) + heightOverhang;
+
+    setCrop(newCrop);
+  };
 
   useGesture(
     {
@@ -25,24 +53,33 @@ const ImageCropper: FC<{ src?: string }> = ({ src }) => {
         setCrop((previousState) => ({ ...previousState, x: dx, y: dy })),
       onPinch: ({ offset: [d] }) =>
         setCrop((previousState) => ({ ...previousState, scale: d })),
+      onDragEnd: adjustImage,
+      onPinchEnd: adjustImage,
     },
-    { target: imageRef, eventOptions: { passive: false } }
+    {
+      target: imageRef,
+      eventOptions: { passive: false },
+      drag: { from: () => [x, y] },
+      pinch: { scaleBounds: { min: 1 } },
+    }
   );
 
   return (
     <>
       <div className="overflow-hidden ring-4 ring-blue-500 aspect-[3/4]">
-        <img
-          src={src}
-          ref={imageRef}
-          className="relative w-auto h-full max-w-none max-h-none"
-          style={{
-            left: x,
-            top: y,
-            touchAction: "none",
-            transform: `scale(${scale})`,
-          }}
-        />
+        <div ref={imageContainerRef} className="w-full h-full">
+          <img
+            src={src}
+            ref={imageRef}
+            className="relative w-auto h-full max-w-none max-h-none"
+            style={{
+              left: x,
+              top: y,
+              touchAction: "none",
+              transform: `scale(${scale})`,
+            }}
+          />
+        </div>
       </div>
       <div className="mt-2">
         <p>Crop X: {x}</p>
